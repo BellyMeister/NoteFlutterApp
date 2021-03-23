612e656dbaee2f500f68fdfedbb642341d957076
Seppi hr. Chef <SHDa.skp@edu.mercantec.dk>
2021-03-23 15:18:48 +0100

    Color setup

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,11 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+        backgroundColor: HexColor.fromHex("#112D52"),
+        primaryColor: HexColor.fromHex("#21E6C1"),
+        accentColor: HexColor.fromHex("#1D416F"),
         primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
